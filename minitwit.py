183d02a7f67cdbcb950d92c9d5912064dd4c7a92
Victor <1-1@hotmail.dk>
2021-02-04 11:22:58 +0100

    Done?

--- minitwit.py
+++ minitwit.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
     MiniTwit
     ~~~~~~~~
@@ -40,7 +38,7 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+            db.cursor().executescript(f.read().decode('utf-8'))
         db.commit()
 
 

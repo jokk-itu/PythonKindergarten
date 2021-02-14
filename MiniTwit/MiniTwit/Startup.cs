15942249e90c86263cbe39f49e894380059edeed
Bjørnar Haugstad Jåtten <bjja@itu.dk>
2021-02-14 12:53:30 +0100

    Finalized front-end (for now)
    Still lacks logic which requires the user to logged in.

--- MiniTwit/MiniTwit/Startup.cs
+++ MiniTwit/MiniTwit/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Net.Http;
-using MiniTwit.Data;
+//using MiniTwit.Data;
 
 namespace MiniTwit
 {

5ec6771a1facb389edbcd8ed688b549d33668b37
Victor <1-1@hotmail.dk>
2021-02-14 13:51:30 +0100

    Move files

--- MiniTwitApi/Server/Program.cs
+++ MiniTwitApi/Server/Program.cs
@@ -21,6 +21,7 @@ namespace MiniTwitApi.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(new string[]{ "http://localhost:5001", "https://localhost:5000"});
                 });
     }
 }

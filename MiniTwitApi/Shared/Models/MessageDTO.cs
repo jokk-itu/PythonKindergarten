817fa11618c9fe82e55f9a9204a25699ddb144c0
Victor <1-1@hotmail.dk>
2021-02-18 10:21:02 +0100

    Add message author

--- MiniTwitApi/Shared/Models/MessageDTO.cs
+++ MiniTwitApi/Shared/Models/MessageDTO.cs
@@ -10,10 +10,11 @@ namespace MiniTwitApi.Shared.Models
 
     public class MessageDTO
     {
-        [JsonProperty("message_id")] public int Id { get; set; }
-        [JsonProperty("author_id")] public int Author { get; set; }
+        [JsonProperty("id")] public int Id { get; set; }
+        [JsonProperty("author")] public int Author { get; set; }
+        [JsonProperty("authorUsername")] public string AuthorUsername { get; set; }
         [JsonProperty("text")] public string Text { get; set; }
-        [JsonProperty("pub_date")] public int PublishDate { get; set; }
+        [JsonProperty("publishDate")] public int PublishDate { get; set; }
         [JsonProperty("flagged")] public int Flagged { get; set; }
     }
 }
